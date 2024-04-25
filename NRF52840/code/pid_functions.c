6344c0eb3cb8d6feea3741515ba33f29e4ea027b
TheodoraLet <145222991+TheodoraLet@users.noreply.github.com>
2024-04-25 18:35:47 +0300

    Update pid_functions.c

--- NRF52840/code/pid_functions.c
+++ NRF52840/code/pid_functions.c
@@ -17,22 +17,23 @@ int ENC_PIN1=26;
 
 int state=1;
 
-int u=0;
+volatile int u=0;
 uint16_t buffer[1];
 int pos_angle;
-int target;
+volatile int target;
 int prev_target;
-int target_angle;
+volatile int target_angle;
 int prev_t_angle;
-int e=0;
+volatile int e=0;
 int eprev=0;
 int eint=0;
 int dedt=0;
 int deltaT=0;
+volatile int pass;
 
-volatile double kp=0.9;
-volatile int kd=0.2;
-volatile int ki=0.2;
+const volatile double kp=0.05;
+const volatile double kd=0.01;
+const volatile double ki=0;
 
 int map(int x,int l1,int h1,int l2,int h2){
 return (int)(x-l1)*(h2-l2)/(h1-l1) +l2;
@@ -41,7 +42,7 @@ return (int)(x-l1)*(h2-l2)/(h1-l1) +l2;
 void pid_calculations(){
 //pos_angle=map(pos,0,20400,0,360);
 target=map(target_angle,0,360,0,40800);
-prev_target=map(prev_t_angle,0,360,0,40800);
+//prev_target=map(prev_t_angle,0,360,0,40800);
 e=pos-target;
 deltaT=time_interval(0); // 32bit width
 dedt=(e-eprev)/deltaT;
@@ -49,6 +50,8 @@ eint+=e*deltaT;
 eprev=e;
 u=kp*e+kd*dedt+ki*eint;
 }
+
+
 
 void Motor_Drive(){
 int temp =(int)fabs(u);
@@ -67,6 +70,7 @@ pin_write(pin2,0);
 pin_write(pin1,0);
 pin_write(pin2,0);
 *buffer=0;
+pass=1;
 }
 pwm_write(pwm_pin,0,buffer);
 }
