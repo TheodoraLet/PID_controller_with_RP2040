7052001ed3785d60fd3b877734252a3a21d231da
TheodoraLet <145222991+TheodoraLet@users.noreply.github.com>
2024-04-25 18:33:48 +0300

    Update main.c

--- NRF52840/code/main.c
+++ NRF52840/code/main.c
@@ -30,21 +30,15 @@ Purpose : Generic application start
 *   Application entry point.
 */
 
-typedef unsigned int uint;
-//#define ENC_PIN1 26
-//#define ENC_PIN2 7
 #define pwm_pin 8
 #define pin1 27
 #define pin2 6
+#define rxd 11
+#define txd 12
 
-void delay(int iter){
-int volatile counter;
-counter=0;
-while(counter<iter){
- ++counter;
-}
 
-}
+uint32_t read_buffer[4];
+extern volatile uint8_t read_size=0;
 
 
 int main(void) {
@@ -52,16 +46,30 @@ __enable_irq();
 pinMode(pin1,1);
 pinMode(pin2,1);
 pwm_set_format(pwm_pin,255);
-
+uart_config(rxd,txd);
+pin_write(pin1,1);
+pin_write(pin2,0);
 timer_init(3,2);
 
 enc_pin(ENC_PIN1,0,ENC_PIN2);
-target_angle=90;
+
+
 
 while(1){
+if(pass){
+uart_read(read_buffer);
+char writebuff[read_size+2];
+for (int i=0;i<=read_size;i++) sprintf(&writebuff[i],"%d",read_buffer[i]-'0');
+
+uart_write(writebuff);
+target_angle=atoi(writebuff);
+pass=0;
+}
+
 pid_calculations();
 
 Motor_Drive();
+
 
 }
 
