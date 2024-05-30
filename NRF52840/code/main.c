8dc9074c0b52b109655cef377eadfde582ecea89
TheodoraLet <145222991+TheodoraLet@users.noreply.github.com>
2024-05-30 11:51:55 +0300

    changes in uart , spi and commentation

--- NRF52840/code/main.c
+++ NRF52840/code/main.c
@@ -1,15 +1,3 @@
-/*********************************************************************
-*                    SEGGER Microcontroller GmbH                     *
-*                        The Embedded Experts                        *
-**********************************************************************
-
--------------------------- END-OF-HEADER -----------------------------
-
-File    : main.c
-Purpose : Generic application start
-
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <nrf52840.h>
@@ -19,61 +7,55 @@ Purpose : Generic application start
 #include "pid_functions.h"
 #include "nrf52840_bitfields.h"
 #include "uart_functions.h"
-#include "interrupt_function.h"
 #include "encoder_function.h"
 #include "timer_functions.h"
-/*********************************************************************
-*
-*       main()
-*
-*  Function description
-*   Application entry point.
-*/
+#include "spi_functions.h"
+#include "encoder_function.h"
+#include "app_uart.h"
+#include "app_error.h"
+#include "nrf_uart.h"
+#include "nrf_delay.h"
 
 #define pwm_pin 8
 #define pin1 27
 #define pin2 6
-#define rxd 11
-#define txd 12
 
 
-uint32_t read_buffer[4];
-extern volatile uint8_t read_size=0;
+int main(void)
+{
+   __enable_irq();
+   // initialize uart driver
+   uart_init();
 
+    
+    int port=0;
+    // initialize fatfs 
+    fatfs_initialization();
+    // set driver pins as output
+    pinMode(pin1,1);
+    pinMode(pin2,1);
+    // initialize pwm duty cycle
+    pwm_set_format(pwm_pin,255);
 
-int main(void) {
-__enable_irq();
-pinMode(pin1,1);
-pinMode(pin2,1);
-pwm_set_format(pwm_pin,255);
-uart_config(rxd,txd);
-pin_write(pin1,1);
-pin_write(pin2,0);
-timer_init(3,2);
+    // set direction of motor (optional)
+    pin_write(pin1,1);
+    pin_write(pin2,0);
+    //initiliaze timer 
+    timer_init(3,2);
+    // initialize encoder pins and GPIOTE event
+    enc_pin(ENC_PIN1,port,ENC_PIN2);
 
-enc_pin(ENC_PIN1,0,ENC_PIN2);
+     while(1){
+     
+     // calculate position and error
+      pid_calculations();
+ 
+     // update the duty cycle to reach target 
+      Motor_Drive();
 
-
-
-while(1){
-if(pass){
-uart_read(read_buffer);
-char writebuff[read_size+2];
-for (int i=0;i<=read_size;i++) sprintf(&writebuff[i],"%d",read_buffer[i]-'0');
-
-uart_write(writebuff);
-target_angle=atoi(writebuff);
-pass=0;
+     // write data to file 
+      fatfs_write();
+     }
 }
 
-pid_calculations();
-
-Motor_Drive();
-
-
-}
-
-
-}
-
-/*************************** End of file ****************************/
+/** @} */
